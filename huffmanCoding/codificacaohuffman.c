b202848c5ddea306ebf6cf9b2ef85e5053c27af2
DanielBretherick <68389495+DanielBretherick@users.noreply.github.com>
2022-06-09 12:41:27 -0300

    atualizado 1
    Arvore criada

--- huffmanCoding/codificacaohuffman.c
+++ huffmanCoding/codificacaohuffman.c
@@ -10,10 +10,34 @@ struct node{
     struct node* r;
 };
 
-int insertQueue(char key, int val, struct node** root){
+
+struct node* createNode(char key, int val){
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
     new_node->key = key;
     new_node->val = val;
+    new_node->l = NULL;
+    new_node->r = NULL;
+    return new_node;
+};
+
+int deleteQueue(struct node** root, char key){
+    struct node* aux = *root;
+    if((*root)->key == key){
+        *root = (*root)->next;
+        return 1;
+    }
+    if((*root)->next == NULL){
+        printf("Elemento nao esta na fila");
+        return 0;
+    }
+    else{
+        deleteQueue(&((*root)->next), key);
+        return 1;
+    }
+}
+
+
+int insertQueue(struct node* new_node, struct node** root){
     if(*root == NULL){
         *root = new_node;
         new_node->next = NULL;
@@ -45,6 +69,42 @@ int insertQueue(char key, int val, struct node** root){
 
 }
 
+struct node* joinNode(struct node* a, struct node* b){
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+
+    new_node->val = (a->val) + (b->val);
+
+    if(a->val >= b->val){
+        new_node->r = a;
+        new_node->l = b;
+        return new_node;
+    }
+
+    new_node->r = b;
+    new_node->l = a;
+    return new_node;
+
+};
+
+struct node* huffmanTree(struct node** root){
+    struct node*aux;
+    if(*root == NULL){
+        printf("Codificacao Vazia\n");
+        return NULL;
+    }
+    if((*root)->next == NULL){
+        return *root;
+    }
+    else{
+        aux = joinNode((*root), (*root)->next);
+        deleteQueue(root, (*root)->key);
+        deleteQueue(root,(*root)->key);
+        insertQueue(aux, root);
+        huffmanTree(root);
+    }
+};
+
+
 void runQueue(struct node* root){
     struct node* aux= root;
     while(aux){
@@ -52,6 +112,18 @@ void runQueue(struct node* root){
         aux = aux->next;
     }
 }
+
+int simetricOrder(struct node* root){
+    if(root->r == NULL && root->l == NULL){
+        printf("%c", root->key);
+        return 1;
+    }
+    simetricOrder(root->l);
+    simetricOrder(root->r);
+}
+
+
+
 void countLetter(char *text, int*cont){
     int i=0;
     while(text[i] != '\0'){
@@ -59,6 +131,10 @@ void countLetter(char *text, int*cont){
         cont[val]++;
         i++;
     }
+}
+
+void codeHuffman(char l, struct node* tree){
+
 }
 
 
@@ -70,15 +146,28 @@ int main(){
     struct node* root;
     root = NULL;
 
-    char text[]= "Daniel Ambrozio Bretherick Marques";
+    char text[]= "dfasdafasfdafasasassasasssss";
 
     countLetter(text, cont);
+
     for(int i=0; i<127; i++){
         if(cont[i] != 0){
-            insertQueue(i,cont[i], &root);
+            printf("%c %d\n", i, cont[i]);
         }
     }
+
+    for(int i = 0; i<127; i++){
+        if(cont[i] != 0){
+            insertQueue(createNode(i, cont[i]), &root);
+        }
+    }
+    printf("\n\n");
+
     runQueue(root);
+    printf("\n\n");
+
+    struct node* aux = huffmanTree(&root);
+    simetricOrder(root);
 
 
     return 0;
